95e9f6a2e82d50ae80e7022c3c51b8a085e184f2
Ollie-D7 <ollie.clark@domain7.com>
2021-09-29 16:41:31 +0100

    Add text amends to opportunities/research page. Add cookie-based notice to header.

--- themes/phe/src/js/footer/scripts.js
+++ themes/phe/src/js/footer/scripts.js
@@ -1,31 +1,37 @@
 (function ($) {
-    
-    const $tabs_toggle = $('.js-tabs-dropdown');
-    const $nav_tabs = $('.nav-tabs--research');
-    const $nav_link = $nav_tabs.find('.nav-link');
+  const $tabs_toggle = $(".js-tabs-dropdown");
+  const $nav_tabs = $(".nav-tabs--research");
+  const $nav_link = $nav_tabs.find(".nav-link");
 
-    $tabs_toggle.on('click', function() {
-        if ( $nav_tabs.hasClass('is-open')) {
-            Close();
-        } else {
-            Open();
-        }
-    });
-
-    $nav_link.on('click', function() {
-        var text = $(this).text();
-        $tabs_toggle.text(text);
-        console.log(text);
-        Close();
-    });
-
-    function Close() {
-        $nav_tabs.removeClass('is-open');
+  $tabs_toggle.on("click", function () {
+    if ($nav_tabs.hasClass("is-open")) {
+      Close();
+    } else {
+      Open();
     }
+  });
 
-    function Open() {
-        $nav_tabs.addClass('is-open');
-    }
+  $nav_link.on("click", function () {
+    var text = $(this).text();
+    $tabs_toggle.text(text);
+    console.log(text);
+    Close();
+  });
 
+  function Close() {
+    $nav_tabs.removeClass("is-open");
+  }
+
+  function Open() {
+    $nav_tabs.addClass("is-open");
+  }
+
+  if (localStorage.getItem("cookieSeen") != "shown") {
+    $(".cookie-banner").addClass("is-active");
+  }
+
+  $(".cookie-banner__close").on("click", function () {
+    $(".cookie-banner").removeClass("is-active");
+    localStorage.setItem("cookieSeen", "shown");
+  });
 })(jQuery);
-  
